38dac566d5607a5f2b6441a562975481eef27932
lyvia medeiros <santosannslyvia@gmail.com>
2021-09-06 18:14:44 -0300

    primeiro commit

---
+++ exerciciohumberto2.py
@@ -0,0 +1,8 @@
+livro = float(input("Qual o valor do seu livro?"))
+quantidade = float(input("Qual a quantidade desejada?"))
+transporte = 3
+transp2 = 0.75
+desconto = 60
+print( " o valor dos exemplares em conjunto com a taxa de frete ficará por:",livro*desconto/100*quantidade+transporte+transp2*59)
+
+
